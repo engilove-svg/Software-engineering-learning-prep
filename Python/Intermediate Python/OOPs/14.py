28b58b4ff0cd62c467e07fbe64c30ea35ef35434
loveleen kaur <engi.asees@gmail.com>
2026-09-14 21:19:05 -0400

    Day 20 Composition

---
+++ Python/Intermediate Python/OOPs/14.py
@@ -0,0 +1,15 @@
+# composition
+class Engine:
+    def start(self):
+        print("Engine started.")
+
+class Car:
+    def __init__(self):
+        self.engine=Engine()#Main step of composition
+
+    def start_car(self):
+        self.engine.start()#Car is telling its contained Engine object to start.
+        print("Car started.")
+
+car=Car()
+car.start_car()
